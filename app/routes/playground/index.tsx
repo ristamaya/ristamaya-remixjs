39edf865dac987779df0f480c9124c1b76a59fad
DilucCafe <diluc.cafe@gmail.com>
2022-04-24 12:05:14 +0700

    add netlify functions

--- app/routes/playground/index.tsx
+++ app/routes/playground/index.tsx
@@ -1,6 +1,6 @@
 function ComponentsShared() {
   return (
-    <div className="m-auto h-full p-5 text-center text-4xl">
+    <div className="m-auto h-full p-5 text-center text-4xl text-theme-base">
       Let's explore my Playground
       <img src="/img/playground.png" className="my-6 mx-auto object-fill" />
       Have Fun!
